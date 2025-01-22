47e2d5c7f60d8031c08adcf381d51af38b7d86af
yuyi <yuyi919606952@foxmail.com>
2025-01-22 22:48:18 +0800

    test

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,7 +3,9 @@ import test from 'ava'
 import { isHiddenFile } from '../index'
 
 test('sync function from native code', (t) => {
-  t.is(isHiddenFile('D:/AlphaDiscLog.txt'), true)
-  t.is(isHiddenFile('D:\\D.zip'), false)
-  t.is(isHiddenFile('D:/workspace'), false)
+  // t.is(isHiddenFile('D:/AlphaDiscLog.txt'), true)
+  // t.is(isHiddenFile('D:\\D.zip'), false)
+  // t.is(isHiddenFile('D:/workspace'), false)
+  // t.is(isHiddenFile('D:\\D.zip'), false)
+  t.is(isHiddenFile(process.cwd()), false)
 })
